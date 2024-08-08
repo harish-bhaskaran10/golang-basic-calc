066ef3e764fbe4f91434904f5d704fc0788f9bd2
Harish Bhaskaran <harishbsk26@gmail.com>
2024-08-08 07:29:14 +0530

    added transient dependecy to calc2

--- calc.go
+++ calc.go
@@ -1,9 +1,19 @@
 package basic_calculator
 
+import calc2 "github.com/harish-bhaskaran10/golang-basic-calc2"
+
 func Addition(a int, b int) int {
 	return a + b
 }
 
 func Subtraction(a int, b int) int {
 	return a - b
+}
+
+func Mul_in_calc(a int, b int) int {
+	return calc2.Multiplication(a, b)
+}
+
+func Div_in_calc(a int, b int) int {
+	return calc2.Division(a, b)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/harish-bhaskaran10/golang-basic-calc
 
 go 1.22.5
+
+require github.com/harish-bhaskaran10/golang-basic-calc2 v0.0.0-20240808015327-8e40417ce5ae // indirect
